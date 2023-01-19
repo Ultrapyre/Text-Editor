05266f9217446813557569ee8292228f4d3db676
Ultrapyre <sidneymakhiutao@yahoo.com.hk>
2023-01-20 00:13:29 +0800

    Configured config... hopefully.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -25,7 +22,20 @@ module.exports = () => {
       }),
       // Generates a manifest.json
       new WebpackPwaManifest({
-
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Scribble notes or code snippets!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('assets/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
       // Pulls in a custom-made service worker from elsewhere.
       new InjectManifest({
